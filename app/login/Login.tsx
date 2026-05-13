ab4fc56a775817fd39a4ff4002af60346abd86f4
Boi <Boibrohihi311@gmai.com>
2026-05-13 17:04:25 +0700

    change ui

--- app/login/Login.tsx
+++ app/login/Login.tsx
@@ -3,7 +3,19 @@ import React, { useState } from 'react'
 import Link from 'next/link'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faEye, faEyeSlash } from "@fortawesome/free-solid-svg-icons"
-import { redirect } from "next/navigation";
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useRouter } from 'next/router'
 const Login = () => {
     const [username, Setusername] = useState("")
     const [password, Setpassword] = useState("")
@@ -18,44 +30,68 @@ const Login = () => {
     const handlerShow = () => {
         SetShowpassword(!showpassword)
     }
+    const handlerSubmit = async() => {
 
+    }
 
     return (
-        <div className="w-screen h-screen overflow-hidden flex items-center justify-around" >
-            <video
-                autoPlay
-                loop
-                muted
-                playsInline
-                className="absolute top-0 left-0 w-full h-full object-cover  -z-20"
-            >
-                <source src="/videos/bg.mp4" type="video/mp4" />
-            </video>
-            <div className='flex max-w-200 bg-[rgba(0,0,0,0.7)] rounded-lg  text-white  flex-col gap-8 px-9 py-10 w-100 aspect-square relative'>
-           <div className="absolute inset-0 -z-10 rounded-xl bg-linear-to-r from-purple-500 to-blue-500 blur-lg opacity-50 "></div>
-            <div>
-                <h1 className='text-[clamp(1.5rem,2vw,2rem)] text-center font-extrabold'>Login</h1>
-            </div>
-            <div className='flex flex-col gap-5 '>
-                <div className='flex flex-col gap-8'>
-                    <input value={username ?? ""} onChange={handlerUser} type="text" placeholder='Username' className=' border border-[rgba(255,255,255,0.5)] rounded-4xl focus:outline-none focus:ring-2 focus:ring-[rgba(255,255,255,0.8)] py-2 px-3 placeholder-white' />
-                    <div className='relative' >
-                        <input value={password ?? ""} onChange={handlerPassword} type={showpassword ? "text" : "password"} placeholder='Password' className='border  border-[rgba(255,255,255,0.5)] rounded-4xl focus:outline-none focus:ring-2 focus:ring-[rgba(255,255,255,0.8)] py-2 px-3 placeholder-white w-full' />
-                        <button type="button" onClick={handlerShow} className='absolute top-2 left-[85%] lg:left-[90%]'>
-                            <FontAwesomeIcon icon={showpassword ? faEye : faEyeSlash} />
-                        </button>
-                    </div>
-                </div>
-                {error != "" ? <p className='text-center text-red-600 font-semibold'>{error}</p> : null}
-                <button className='bg-white text-black rounded-4xl py-3  cursor-pointer' >Login</button>
-                <div className='flex justify-center'>
-                    Don't have account?
-                    <Link className='font-semibold hover:underline' href={"/register"}>Register</Link>
-                </div>
-            </div>
+        <div className="w-screen h-screen overflow-hidden flex items-center justify-around bg-neutral-950" >
+            <Card className="w-full max-w-sm mx-auto bg-neutral-900">
+                <CardHeader className='text-white'>
+                    <CardTitle>Login</CardTitle>
+                    <CardAction>
+                        <a href="register" className='hover:underline text-[16px]'>
+                                Register
+                            </a>
+                    </CardAction>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <div className="flex flex-col gap-6 text-white">
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="username">Username</Label>
+                                </div>
+                                <Input onChange={(e) => handlerUser(e)} value={username} id="username" required className='text-white border-white/40' />
+                            </div>
+                            <div className="grid gap-2">
+                                <div className="flex items-center">
+                                    <Label htmlFor="password">Password</Label>
+                                    <a
+                                        href="#"
+                                        className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
+                                    >
+                                        Forgot your password?
+                                    </a>
+                                </div>
+                                <div className="relative">
+                                    <Input id="password" value={password} type={showpassword ? "text" : "password"} required className='text-white border-white/40 ' onChange={(e) => handlerPassword(e)} />
+                                    <button type="button" onClick={handlerShow} className='absolute top-2 left-[85%] lg:left-[90%]'>
+                                        <FontAwesomeIcon icon={showpassword ? faEye : faEyeSlash} />
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+                    </form>
+                    <CardDescription className='text-red-500'>
+                        {error}
+                    </CardDescription>
+                </CardContent>
+                <CardFooter className="flex-col gap-2">
+                    <Button onClick={handlerSubmit} type="submit" className="w-full bg-white text-black">
+                        Dang nhap
+                    </Button>
+                    <Link href={'/home'} className='w-full'>
+                        <Button type="submit" className="w-full border-white/40 border text-white">
+                            Quay ve trang chu
+                        </Button>
+                    </Link>
+                </CardFooter>
+            </Card>
         </div>
-            
-        </div>
+
     )
 }
 
