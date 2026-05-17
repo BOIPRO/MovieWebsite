e3875dff66b18b241150037c11ec6d00f3c6896a
Boi <Boibrohihi311@gmai.com>
2026-05-17 17:40:24 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="flex h-screen items-center justify-center bg-neutral-950">
+      <h1 className="text-3xl font-bold text-neutral-50">404 - Page Not Found</h1>
+    </div>
+  )
+}
