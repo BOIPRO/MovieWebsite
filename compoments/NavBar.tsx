f3c10acb2cdb7f38cbe967bb1b5a80a0d3ee47f1
Boi <Boibrohihi311@gmai.com>
2026-05-18 20:16:04 +0700

    feature add icon profile and get userinfo by cookies

--- compoments/NavBar.tsx
+++ compoments/NavBar.tsx
@@ -1,12 +1,31 @@
 "use client"
-import { useState } from 'react'
+import { useState} from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import Image from 'next/image'
 import { faMagnifyingGlass, faArrowRightToBracket, faBars } from "@fortawesome/free-solid-svg-icons"
 import Link from 'next/link'
 import Menu from './Menu'
 import { useRouter } from 'next/navigation'
-const NavBar = () => {
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar"
+interface Prop {
+    user : {
+        username : string
+    }
+}
+const NavBar = ({ user }: Prop) => {
     const [openMenu, SetopenMenu] = useState(false);
     const [openSearch, SetopenSearch] = useState(false);
     const [contextSearch, SetcontextSearch] = useState("");
@@ -36,14 +55,37 @@ const NavBar = () => {
                         <input value={contextSearch ? contextSearch : ""} onKeyDown={handleKeyDown} onChange={handleSearch} className='w-full rounded-2xl border px-10 h-10 focus:rounded-lg transition-all duration-300 ease-in' type="text" placeholder='Tìm kiếm phim' />
                         <FontAwesomeIcon className='absolute top-1/2 -translate-y-1/2 left-1.5 ' icon={faMagnifyingGlass} />
                     </div>
-                    <div>
-                        <Link href={'/login'}>
-                            <div className='flex flex-col gap-1 bg-slate-800 rounded-md text-[16px]  py-1 px-4 justify-center items-center text-navbar cursor-pointer'>
-                                <FontAwesomeIcon icon={faArrowRightToBracket} />
-                                <p>Đăng nhập</p>
-                            </div>
+                    {user.username ? (
+                        <div className='text-navbar flex gap-2 items-center'>
+                            <p className='text-[16px]'>{user.username}</p>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="cursor-pointer" >
+                                    <Avatar size='lg' >
+                                        <AvatarImage
+                                            src="https://github.com/shadcn.png"
+                                            alt="@shadcn"
+                                            className="grayscale"
+                                        />
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className='bg-blue backdrop-blur-sm border-white/20 text-white' >
+                                    <DropdownMenuGroup>
+                                        <DropdownMenuLabel>{user.username}</DropdownMenuLabel>
+                                        <DropdownMenuItem>Profile</DropdownMenuItem>
+                                        <DropdownMenuItem >Logout</DropdownMenuItem>
+                                    </DropdownMenuGroup>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </div>
+
+                    ) : (
+                        <Link href={'/login'} className='flex flex-col gap-1 bg-blue-800 rounded-2xl text-[16px]  py-2 px-4 justify-center items-center text-navbar cursor-pointer'>
+                            <FontAwesomeIcon icon={faArrowRightToBracket} />
+                            <p>Đăng nhập</p>
                         </Link>
-                    </div>
+                    )}
+
                 </section>
                 <section className='xl:hidden flex-col px-2 bg-slate-950 justify-center border-t-white/50 border-t-[0.2px] '>
                     <div className='flex text-navbar justify-between py-4 text-[20px]'>
